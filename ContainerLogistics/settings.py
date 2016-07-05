39ada18c735d6765fc3c9d6329292a276092046c
rhetthegreat <rhettdsouza13@gmail.com>
2016-07-05 11:11:02 +0530

    Tweaks to Registration

--- ContainerLogistics/settings.py
+++ ContainerLogistics/settings.py
@@ -99,7 +99,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/dashboard/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
